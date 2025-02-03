693e903ccb783908ab2c377afa17b78767685243
Niel22 <niel2264@gmail.com>
2025-02-03 10:10:41 +0100

    Commited 10:10am

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,31 @@
 <template>
     <h1>{{ greet }} {{ name }}</h1>
     <h2 v-html="channel"></h2>
-    <h2 v-bind:id="headingId">Heading</h2>
-    <button v-bind:disabled="isDisabled">Hello world</button>
+    <h2 :id="headingId">Heading</h2>
+    <button :disabled="isDisabled">Hello world</button>
     <h2 class="underline">Underline Text</h2>
-    <h2 v-bind:class="status">Status</h2>
-    <h2 v-bind:class="isPromoted ? 'promoted' : ''">promoted Movie</h2>
-    <h2 v-bind:class="isSoldOut ? 'sold-out' : 'new'">Sold Out Movie</h2>
-    <h2 v-bind:class="isPromoted ? ['new', 'promoted'] : 'sold-out'">Newly Promoted Movie</h2>
+    <h2 :class="status">Status</h2>
+    <h2 :class="isPromoted ? 'promoted' : ''">promoted Movie</h2>
+    <h2 :class="isSoldOut ? 'sold-out' : 'new'">Sold Out Movie</h2>
+    <h2 :class="isPromoted ? ['new', 'promoted'] : 'sold-out'">Newly Promoted Movie</h2>
+    <h2 :class="{
+      promoted: isPromoted,
+      new: !isSoldOut,
+      'sold-out': isSoldOut
+    }">Object conditional Movie</h2>
+
+    <h2 :style="{
+      color: highlightColor,
+      fontSize: size+'px',
+      padding: '20px',
+      border: '1px solid red'
+    }">Inline Style</h2>
+
+    <h2 :style="styleObject">Another Style</h2>
+
+    <h1 :style="[baseStyle, successStyle]">Another of another style</h1>
+
+    
 </template>
 
 <script>
@@ -23,7 +41,24 @@ export default {
       isDisabled: true,
       status: "danger",
       isPromoted: true,
-      isSoldOut: false
+      isSoldOut: true,
+      highlightColor: 'orange',
+      size: 50,
+      styleObject: {
+        color: 'blue',
+        fontSize: '90px',
+        padding: '10px',
+        border: '1px solid yellow'
+      },
+      baseStyle: {
+        fontSize: '30px',
+        fontWeight: 'bold'
+      },
+      successStyle: {
+        color: 'green',
+        backgroundColor: 'black',
+        border: '1px solid red'
+      }
     }
   }
 }
